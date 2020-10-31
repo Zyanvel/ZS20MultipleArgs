f04c8614d9f3a2c75f783094fff77b73c972a333
Zyanvel <zyanvel@gmail.com>
2020-10-30 17:13:18 -0700

    multiple args first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function isSnakeEyes(num1, num2) {
+    if (num1 === 1 && num2 === 1) {
+        console.log("Snake Eyes!! >:D");
+    } else {
+        console.log("Not Snake Eyes!! :D");
+    }
+}
+
+isSnakeEyes(1,2);
